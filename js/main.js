8becdd84f8bd639c6a9fe9422bd261293d079fa1
melfilin <olga.volodina.web@gmail.com>
2021-07-12 18:32:29 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  effect: 'fade',
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
