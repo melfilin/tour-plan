6026b36cfb09671340472d6d0553f3c7dd0b2f6b
melfilin <olga.volodina.web@gmail.com>
2021-07-15 22:13:34 +0300

    create: added parallax effect and fixed review block

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
-
+  autoHeight: true,
   // Navigation arrows
   navigation: {
     nextEl: '.hotel-slider__button--next',
@@ -17,7 +17,7 @@ const hotelSlider = new Swiper('.hotel-slider', {
 const reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
-
+  autoHeight: true,
   // Navigation arrows
   navigation: {
     nextEl: '.reviews-slider__button--next',
@@ -28,3 +28,5 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     keyboardOptions: true,
   },
 });
+
+$('.newsletter').parallax({ imageSrc: '../img/newsletter-bg.jpg' });
