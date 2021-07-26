f83d252071cc72d36bc1bc068202778668862cf4
melfilin <olga.volodina.web@gmail.com>
2021-07-26 09:04:47 +0300

    fixed adaptive version packages  and set up sending letters

---
+++ send.php
@@ -0,0 +1,65 @@
+
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+if ($email) {
+    $body = "
+    <h2>Новое обращение</h2>
+    <b>Почта:</b> $email<br>
+    ";
+}
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 4;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'olga.volodina.web@gmail.com'; // Логин на почте
+    $mail->Password   = 'vxgh3325xg'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('olga.volodina.web@gmail.com', 'Ольга Володина'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('olga.volodina.97@mail.ru');  
+    
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+
+    } catch (Exception $e) {
+        $result = "error";
+        $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+    }
+
+    // Отображение результата
+    header('Location: thankyou.html');
