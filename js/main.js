507e0b83e9cb8d4d0f86e7c92b0d32c51e15c54a
melfilin <olga.volodina.web@gmail.com>
2021-07-26 13:26:51 +0300

    create: added modal window

--- js/main.js
+++ js/main.js
@@ -1,39 +1,68 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: 'fade',
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: 'fade',
 
-  keyboard: {
-    keyboardOptions: true,
-  },
-});
+    keyboard: {
+      keyboardOptions: true,
+    },
+  });
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  autoHeight: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    autoHeight: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
 
-  keyboard: {
-    keyboardOptions: true,
-  },
-});
+    keyboard: {
+      keyboardOptions: true,
+    },
+  });
 
-$('.newsletter').parallax({ imageSrc: 'img/newsletter-bg.jpg' });
+  $('.newsletter').parallax({ imageSrc: 'img/newsletter-bg.jpg' });
 
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  console.log('Клик по кнопке меню');
-  document
-    .querySelector('.navbar-bottom')
-    .classList.toggle('navbar-bottom--visible');
+  var menuButton = $('.menu-button');
+  menuButton.on('click', function () {
+    $('.navbar-bottom').toggleClass('navbar-bottom--visible');
+  });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  $('body').on('keydown', closeModalByEsc);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+
+  function closeModalByEsc(event) {
+    if (event.keyCode && event.keyCode == 27) {
+      var modalOverlay = $('.modal__overlay');
+      var modalDialog = $('.modal__dialog');
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    }
+  }
 });
