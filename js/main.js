2ff87ce6db8d895238506b16174d1df31dbdea80
melfilin <olga.volodina.web@gmail.com>
2021-07-27 15:10:15 +0300

    create:added form validation

--- js/main.js
+++ js/main.js
@@ -65,4 +65,26 @@ $(document).ready(function () {
       modalDialog.removeClass('modal__dialog--visible');
     }
   }
+
+  // Обработка форм
+  $('.form').each(function () {
+    $(this).validate({
+      errorClass: 'invalid',
+      messages: {
+        name: {
+          required: 'Please enter your name',
+          minlength: 'Name must be at least 2 letters long',
+        },
+        email: {
+          required: 'We need your email address to contact you',
+          email: 'Your email address must be in the format of name@domain.com',
+        },
+        phone: {
+          required: 'Phone is required',
+        },
+      },
+    });
+  });
+
+  $('input[name="phone"]').mask('+7 (999) 999-99-99');
 });
